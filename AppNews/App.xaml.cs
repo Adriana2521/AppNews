16e05dacac0e4b303fff5f3d5a3ff3e8905aa45c
181G0258@rcarbonifera.tecnm.mx <181G0258@rcarbonifera.tecnm.mx>
2023-06-14 23:30:02 -0600

    Enrutamento agregado

--- AppNews/App.xaml.cs
+++ AppNews/App.xaml.cs
@@ -1,12 +1,20 @@
 ﻿namespace AppNews;
+using AppNews.Views;
 using AppNews.Views.Usuarios;
-
+using AppNews.Views.Administrador;
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
+        #region Rutas
+        Routing.RegisterRoute("//principal", typeof(PrincipalView));
+        Routing.RegisterRoute("//login", typeof(LoginView));
+        Routing.RegisterRoute("//principaladmin", typeof(PrincipalViewAD));
+        Routing.RegisterRoute("//addnoticia", typeof(AgregarNoticiaView));
+        Routing.RegisterRoute("//editnoticia", typeof(EditarNoticiaView));
+        Routing.RegisterRoute("//userview", typeof(NoticiasView));
+		#endregion
 		MainPage = new NoticiasView();
 	}
 }
